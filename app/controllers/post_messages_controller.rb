4b872daaca7aeac5d88022e80727326767339fe2
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-02 09:26:12 +0000

    投稿機能(データ操作)作成。動作確認のため_form.html.erbのform_with urlを一時的に変えている

--- app/controllers/post_messages_controller.rb
+++ app/controllers/post_messages_controller.rb
@@ -5,10 +5,26 @@ class PostMessagesController < ApplicationController
   def new
     @post_message = PostMessage.new
   end
+  def create
+    @post_message = PostMessage.new(post_message_params)
+    if params[:back]
+      render :new
+    else
+      if @post_message.save
+        redirect_to post_messages_path, notice: "ブログを作成しました！"  #一覧画面へ遷移
+      else
+        render :new  #同画面を再描画
+      end
+    end
+  end
 
   def edit
   end
 
   def confirm
+  end
+  private
+  def post_message_params
+    params.require(:post_message).permit(:title, :content)
   end
 end
